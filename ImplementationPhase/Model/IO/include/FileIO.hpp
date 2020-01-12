4eea06b6f8b8f8950870773d4073e39fb59ebe8f
Johannes Häring <johannes.haering99@gmx.de>
2020-01-12 11:33:48 +0100

    Removed singleton from fileio

--- ImplementationPhase/Model/IO/include/FileIO.hpp
+++ ImplementationPhase/Model/IO/include/FileIO.hpp
@@ -5,13 +5,8 @@
 
 class FileIO
 {
-public:
-    static FileIO getInstance();
 protected:
     virtual std::string readPureFile(std::string path);
     virtual bool writePureFile(std::string path, std::string output);
-
-private:
-    static FileIO singleton;
 };
 #endif
