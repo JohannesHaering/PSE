65e3fd000d93cf7ed40cd30dfff4dafda5fca33c
Manuel Drehwald <ufpoa@student.kit.edu>
2020-01-16 23:41:08 +0100

    update CMake to change usage from stubs to real code

--- ImplementationPhase/Controller/CommunicationModule/include/Channel.hpp
+++ ImplementationPhase/Controller/CommunicationModule/include/Channel.hpp
@@ -2,19 +2,28 @@
 #define CHANNEL_H_
 #include <list>
 
+#include <opencv2/opencv.hpp>
+
+#include "NeuralNetworkAdapter.hpp"
+#include "Result.hpp"
+#include "Device"
+
+
 // Abstract class. Should be extended by device-type specific classes.
 class Channel {
   PUBLIC:
     Channel(Device connectedDevice);
-    Channel(Device connectedDevice, std::list<NeuralNetwork> neuralNetworkList);
-    void setNeuralNetworkList(std::list<NeuralNetwork> neuralNetworkList);
+    Channel(Device connectedDevice, std::list<NeuralNetworkAdapter> neuralNetworkList);
+    void setNeuralNetworkList(std::list<NeuralNetworkAdapter> neuralNetworkList);
     std::list<Result> sendImageList(std::list imageList);
 
   PROTECTED:
-    virtual void Channel::setNeuralNetworkList(std::list<NeuralNetwork> neuralNetworkList) = 0;
+    virtual void Channel::setNeuralNetworkList(std::list<NeuralNetworkAdapter> neuralNetworkList) = 0;
     
 
   PRIVATE:
     Device deviceHandler;
     std::list<NeuralNetworkAdapter> neuralNetworkList;
     std::list<cv::Mat> imageList;
+};
+#endif
