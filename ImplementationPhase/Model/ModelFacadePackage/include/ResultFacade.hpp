a7145531d84301885fe478343cadfea927a61e6e
Johannes Häring <johannes.haering99@gmx.de>
2020-01-18 11:24:32 +0100

    Added write back funcionality to the result facade

--- ImplementationPhase/Model/ModelFacadePackage/include/ResultFacade.hpp
+++ ImplementationPhase/Model/ModelFacadePackage/include/ResultFacade.hpp
@@ -12,7 +12,7 @@ class ParseResultFacade : public ModelFacade
 public:
     DetectionResult parseDetectionResult(std::string resultString);
     ClassificationResult parseClassificationResult(std::string resultString);
-    bool writeDetectionResult(DetectionResult result);
-    bool writeClassificationResult(ClassificationResult result);
+    bool writeDetectionResult(DetectionResult result, std::string path);
+    bool writeClassificationResult(ClassificationResult result, std::string path);
 };
 #endif
--- ImplementationPhase/Model/ModelFacadePackage/src/ResultFacade.cpp
+++ ImplementationPhase/Model/ModelFacadePackage/src/ResultFacade.cpp
@@ -3,6 +3,8 @@
 #include "ClassificationResultParser.hpp"
 #include "DetectionResult.hpp"
 #include "ClassificationResult.hpp"
+#include "TextFileIO.hpp"
+#include "Data.hpp"
 
 #include <string>
 
@@ -16,10 +18,14 @@ ClassificationResult ParseResultFacade::parseClassificationResult(std::string re
     return ClassificationResultParser().parse(resultString);
 }
 
-bool writeDetectionResult(DetectionResult result)
+bool writeDetectionResult(DetectionResult result, std::string path)
 {
+    std::string output = DetectionResultParser().parseBack(result);
+    return TextFileIO().writeFile(path, Data(output));
 }
 
-bool writeClassificationResult(ClassificationResult result)
+bool writeClassificationResult(ClassificationResult result, std::string path)
 {
+    std::string output = ClassificationResultParser().parseBack(result);
+    return TextFileIO().writeFile(path, Data(output));
 }
