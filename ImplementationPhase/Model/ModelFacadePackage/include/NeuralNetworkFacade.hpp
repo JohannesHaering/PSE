37d359da89fe30f0b59524ad849291764c5e3ef2
Johannes Häring <johannes.haering99@gmx.de>
2020-01-19 15:44:28 +0100

    Added loading and savin of multiple neural networks

--- ImplementationPhase/Model/ModelFacadePackage/include/NeuralNetworkFacade.hpp
+++ ImplementationPhase/Model/ModelFacadePackage/include/NeuralNetworkFacade.hpp
@@ -5,11 +5,14 @@
 #include "ModelFacade.hpp"
 
 #include <string>
+#include <list>
 
 class NeuralNetworkFacade : public ModelFacade
 {
 public:
     NeuralNetwork loadNeuralNetwork(std::string path);
+    std::list<NeuralNetwork> loadNeuralNetworks(std::list<std::string> paths);
     bool saveNeuralNetwork(NeuralNetwork neuralNetwork, std::string path);
+    bool saveNeuralNetworks(std::list<NeuralNetwork> neuralNetworks, std::list<std::string> paths);
 };
 #endif
