c5d7960d14fb1facbd09b556dffa3fc96b80256e
Stefi <stefani.guneshka@abv.bg>
2020-01-23 17:18:32 +0100

    changed the cmake structure

---
+++ View/CMakeFiles/Project_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_autogen"
+  "Project_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Project_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
