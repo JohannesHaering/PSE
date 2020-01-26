e64a066c0660aad09a29838934ee9d1e6ac4e7d2
dimitxx <dimitxx@gmail.com>
2020-01-26 12:31:42 +0100

    Update PredictionHandlerTest.cpp

--- ImplementationPhase/test/Controller/EventHandlerModule/PredictionHandlerTest.cpp
+++ ImplementationPhase/test/Controller/EventHandlerModule/PredictionHandlerTest.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "PredictionHandler.hpp"
-#include "inferencepageadapter.h"
+#include "InferencePageAdapter.hpp"
 
 class InferencerPredictionMock : public InferencePageAdapter
 {
