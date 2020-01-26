e9ef2150efd650d53905f602340ae92c262c91b2
Johannes Häring <johannes.haering99@gmx.de>
2020-01-26 22:37:05 +0100

    Added unit tests for text io

--- ImplementationPhase/test/Model/IO/Data_test.cpp
+++ ImplementationPhase/test/Model/IO/Data_test.cpp
@@ -1,19 +1,26 @@
 #include "Data.hpp"
 
 #include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
 #include <string>
 
-TEST (DataTest, string) {
-    Data<std::string> data = Data("foo");
+
+TEST (DataTest, stringO) {
+    Data<std::string> data = Data<std::string>("foo");
     EXPECT_EQ("foo", data.getData());
 }
 
-TEST (DataTest, float) {
-    Data<float> data = Data(3.1);
+TEST (DataTest, floatO) {
+    Data<float> data = Data<float>(3.1);
     EXPECT_EQ(3.1, data.getData());
 }
 
-int main(int argc, char ∗∗argv) {
+TEST (DataTest, nullO) {
+    Data<std::string*> data = Data<std::string*>(nullptr);
+    EXPECT_EQ(nullptr, data.getData());
+}
+
+int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
--- ImplementationPhase/test/Model/IO/TextFileIO_test.cpp
+++ ImplementationPhase/test/Model/IO/TextFileIO_test.cpp
@@ -1,11 +1,56 @@
 #include "TextFileIO.hpp"
 #include <gtest/gtest.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Data.hpp"
 
 TEST(readFileTest, emptyPath){
-    
+    auto io = TextFileIO();
+    auto input = io.readFile("");
+    EXPECT_EQ("", input.getData());
 }
 
-int main(int argc, char ∗∗argv) {
+TEST(readFileTest, file){
+    auto io = TextFileIO();
+    auto input = io.readFile("..\\..\\Data\\testdata\\testTXT.txt");
+    EXPECT_EQ("foo", input.getData());
+}
+
+TEST(readFileTest, noTextFile){
+    auto io = TextFileIO();
+    auto input = io.readFile("..\\..\\Data\\testBMP.bmp");
+    EXPECT_EQ("", input.getData());
+}
+
+TEST(readFileTest, notExisting) {
+  auto io = TextFileIO();
+  EXPECT_THROW(io.readFile("..\\..\\Data\\NotExisting.txt"), std::ifstream::failure);
+}
+TEST(writeFileTest, emptyPath){
+    auto io = TextFileIO();
+    auto success = io.writeFile("", Data<std::string>("foo"));
+    EXPECT_EQ(false, success);
+}
+TEST(writeFileTest, emptyData){
+    auto io = TextFileIO();
+    auto success = io.writeFile("..\\..\\Data\\testdata\\testTXT.txt", Data<std::string>(""));
+    EXPECT_EQ(false, success);
+}
+
+TEST(writeFileTest, file){
+    auto io = TextFileIO();
+    auto success = io.writeFile("..\\..\\Data\\testdata\\testTXT.txt", Data<std::string>("foo"));
+    EXPECT_EQ(true, success);
+}
+
+TEST(writeFileTest, noTextFile){
+    auto io = TextFileIO();
+    auto success = io.writeFile("..\\..\\Data\\testBMP.bmp", Data<std::string>("foo"));
+    EXPECT_EQ(false, success);
+}
+
+int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
