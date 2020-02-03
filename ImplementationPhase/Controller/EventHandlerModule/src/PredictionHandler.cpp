1a9702f7ef62d1d2474f31b256703fb85dcd1552
steki555 <stefani.guneshka@abv.bg>
2020-02-03 15:40:56 +0100

    Done some opencl tests

--- ImplementationPhase/Controller/EventHandlerModule/src/PredictionHandler.cpp
+++ ImplementationPhase/Controller/EventHandlerModule/src/PredictionHandler.cpp
@@ -50,42 +50,8 @@ void PredictionHandler::onAction(){
 		powerPrediction += it->second;
 	}
 
-	performancePrediction = this->test();
-
     page->setPerformancePrediction(performancePrediction);
     page->setPowerPrediction(powerPrediction);
 
     page->update();
-}
-
-float PredictionHandler::test() {
-	// Setup OpenCl
-	glGetDeviceIDs(NULL, CL_DEVICE_TYPE_CPU, 1, &device, NULL);
-	auto context = clCreateContext(NULL, 1, &device, NULL, NULL, NULL);
-	auto queue = clCreateCommandQueue(context, device, (cl_command_queue_properties)0, NULL);
-
-	char *code = {
-		"kernel void calc(global float *data) {\n"
-		"   int id = get_global_id(0);\n"
-		"   data[id] = 10 * 4.2f;\n"
-		"}\n"
-	};
-
-	auto program = clCreateProgramWithSourceCode(context, 1, (const char**)&code, NULL, NULL);
-	clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
-	auto kernel = clCreateKernel(program, "calc", NULL);
-
-	auto buffer = clCreateKernel(context, CL_MEM_READ_WRITE, DATA_SIZE, NULL, NULL);
-
-	clEnqueueWriteBuffer(queue, buffer, CL_FALSE, 0, DATA_SIZE, data, 0, NULL, NULL);
-
-	clSetKernelArg(kernel, 0, sizeof(buffer), &buffer);
-	size_t global_dimensions[] = { LENGTH, 0, 0 };
-	clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_dimensions, NULL, 0, NULL, NULL);
-
-	clEnqueueReadBuffer(queue, buffer, CL_FALSE, 0, sizeof(cl_float)*LENGTH, data, 0, NULL, NULL);
-
-	clFinish(queue);
-
-	return data[0];
 }
