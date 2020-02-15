00e8101ee1d879942e8eb65c1e92e5d7f29a04a3
Johannes Häring <johannes.haering99@gmx.de>
2020-02-15 17:33:39 +0100

    Added get image method for tensors

--- ImplementationPhase/Model/ModelFacadePackage/include/ImageFacade.hpp
+++ ImplementationPhase/Model/ModelFacadePackage/include/ImageFacade.hpp
@@ -2,6 +2,7 @@
 #define IMAGE_FACADE_H_
 
 #include "ModelFacade.hpp"
+#include "MatrixDefine.hpp"
 
 #include <opencv2/opencv.hpp>
 #include <string>
@@ -9,14 +10,15 @@
 #include <vector>
 
 class ImageFacade : public ModelFacade {
-    public:
-        std::vector<float> getImageGreyScale(std::string path, int width, int height, int channelNumb);
-        cv::Mat getImage(std::string path, int width, int height, int channelNumb);
-        std::list<cv::Mat> getImages(std::list<std::string> paths, int width, int height, int channelNumb);
-        std::list<cv::Mat> getImagesFromTxtFile(std::string path, int width, int height, int channelNumb);
-        cv::Mat getImageFromCamera(int deviceId,  int width, int height, int channelNumb);
-        std::list<cv::Mat> getImagesFromVideo(std::string path, int width, int height, int channelNumb);
-        bool writeImage(std::string path, cv::Mat image);
-        bool writeImages(std::list<std::string> paths, std::list<cv::Mat> images);
+public:
+	std::vector<float> getImageGreyScale(std::string path, int width, int height, int channelNumb);
+	cv::Mat getImage(std::string path, int width, int height, int channelNumb);
+	std::list<cv::Mat> getImages(std::list<std::string> paths, int width, int height, int channelNumb);
+	std::list<cv::Mat> getImagesFromTxtFile(std::string path, int width, int height, int channelNumb);
+	cv::Mat getImageFromCamera(int deviceId, int width, int height, int channelNumb);
+	std::list<cv::Mat> getImagesFromVideo(std::string path, int width, int height, int channelNumb);
+	std::vector<MatrixDefine::TENSOR(float)> getImages(std::string directory, int batchSize, int width, int height);
+	bool writeImage(std::string path, cv::Mat image);
+	bool writeImages(std::list<std::string> paths, std::list<cv::Mat> images);
 };
 #endif
--- ImplementationPhase/Model/ModelFacadePackage/src/ImageFacade.cpp
+++ ImplementationPhase/Model/ModelFacadePackage/src/ImageFacade.cpp
@@ -6,6 +6,7 @@
 #include "VideoHandler.hpp"
 #include "ImageParserWithSizing.hpp"
 #include "ImageFacade.hpp"
+#include "MatrixDefine.hpp"
 
 #include <opencv2/opencv.hpp>
 #include <string>
@@ -14,63 +15,96 @@
 
 cv::Mat ImageFacade::getImage(std::string path, int width, int height, int channelNumb)
 {
-    cv::Mat image = ImageFileIO().readFile(path).getData();
-    cv::Mat parsedImage;
-    if(width != 0)
+	cv::Mat image = ImageFileIO().readFile(path).getData();
+	cv::Mat parsedImage;
+	if (width != 0)
 		parsedImage = ImageParserWithSizing(width, height, channelNumb).parse(image);
-    else 
+	else
 		parsedImage = image;
-    return parsedImage;
+	return parsedImage;
 }
 
 std::list<cv::Mat> ImageFacade::getImages(std::list<std::string> paths, int width, int height, int channelNumb)
 {
-    std::list<cv::Mat> images;
-    for (auto it = paths.begin(); it != paths.end(); ++it)
-    {
-        images.push_back(getImage(*it, width, height, channelNumb));
-    }
-    return images;
+	std::list<cv::Mat> images;
+	for (auto it = paths.begin(); it != paths.end(); ++it)
+	{
+		images.push_back(getImage(*it, width, height, channelNumb));
+	}
+	return images;
+}
+
+std::vector<TENSOR(float)> ImageFacade::getImages(std::string directory, int batchSize, int width, int height, int channelNumb) {
+	std::list<std::string> paths = ModelFacade::readDirectory(directory);
+	std::vector<std::vector<float>> images = std::vector<std::vector<float>>();
+
+	for (std::list<std::string>::iterator it = paths.begin(); it != paths.end(); ++it)
+		images.push_back(getImageGreyScale(*it, width, height, channelNumb));
+
+	std::vector<TENSOR(float)> tensors = std::vector<TENSOR(float)>();
+
+	int batch = 0;
+	std::vector<std::vector<std::vector<std::vector<float>>>> tensor = std::vector<std::vector<std::vector<std::vector<float>>>>();
+	for (int i = 0; i < images.size(); i++) {
+		std::vector<std::vector<std::vector<float>>> v = std::vector<std::vector<std::vector<float>>>();
+		std::vector<std::vector<float>> image = std::vector<std::vector<float>>();
+		for (int y = 0; y < height; y++) {
+			std::vector<float> row = std::vector<float>();
+			for (int x = 0; x < width; x++) {
+				row.push_back(images[i][x + y * width]);
+			}
+			image.push_back(row);
+		}
+		v.push_back(image);
+		tensor.push_back(v);
+
+		if (i % batchSize == 0 && batchSize > 0) {
+			tensors.push_back(tensor);
+			tensor = std::vector<std::vector<std::vector<std::vector<float>>>>();
+		}
+	}
+
+	return tensors;
 }
 
 std::list<cv::Mat> ImageFacade::getImagesFromTxtFile(std::string path, int width, int height, int channelNumb)
 {
-    std::list<std::string> paths = ModelFacade::readDirectory(path);
-    return getImages(paths, width, height, channelNumb);
+	std::list<std::string> paths = ModelFacade::readDirectory(path);
+	return getImages(paths, width, height, channelNumb);
 }
 
 cv::Mat ImageFacade::getImageFromCamera(int deviceId, int width, int height, int channelNumb)
 {
-    auto cameraHandler = CameraHandler(deviceId, 30);
-    cv::Mat capturedImage = cameraHandler.getNextFrame();
-    cv::Mat parsedCapturedImage = ImageParserWithSizing(width, height, channelNumb).parse(capturedImage);
-    return parsedCapturedImage;
+	auto cameraHandler = CameraHandler(deviceId, 30);
+	cv::Mat capturedImage = cameraHandler.getNextFrame();
+	cv::Mat parsedCapturedImage = ImageParserWithSizing(width, height, channelNumb).parse(capturedImage);
+	return parsedCapturedImage;
 }
 
 std::list<cv::Mat> ImageFacade::getImagesFromVideo(std::string path, int width, int height, int channelNumb)
 {
-    auto video = VideoFileIO().readFile(path).getData();
-    auto videoHandler = VideoHandler(1);
-    std::list<cv::Mat> frames = videoHandler.handleVideo(video);
-    std::list<cv::Mat> parsedFrames;
-    for (auto it = frames.begin(); it != frames.end(); ++it)
-    {
-        parsedFrames.push_back(ImageParserWithSizing(width, height, channelNumb).parse(*it));
-    }
-    return parsedFrames;
+	auto video = VideoFileIO().readFile(path).getData();
+	auto videoHandler = VideoHandler(1);
+	std::list<cv::Mat> frames = videoHandler.handleVideo(video);
+	std::list<cv::Mat> parsedFrames;
+	for (auto it = frames.begin(); it != frames.end(); ++it)
+	{
+		parsedFrames.push_back(ImageParserWithSizing(width, height, channelNumb).parse(*it));
+	}
+	return parsedFrames;
 }
 
 bool ImageFacade::writeImage(std::string path, cv::Mat image)
 {
-    return ImageFileIO().writeFile(Data<cv::Mat>(image), path);
+	return ImageFileIO().writeFile(Data<cv::Mat>(image), path);
 }
 
-bool ImageFacade::writeImages(std::list<std::string> paths, std::list<cv::Mat> images){
-    return MultipleImageFileIO().writeFile(Data<std::list<cv::Mat>>(images), paths);
+bool ImageFacade::writeImages(std::list<std::string> paths, std::list<cv::Mat> images) {
+	return MultipleImageFileIO().writeFile(Data<std::list<cv::Mat>>(images), paths);
 }
 
 std::vector<float> ImageFacade::getImageGreyScale(std::string path, int width, int height, int channels) {
-    cv::Mat image = ImageFileIO().readFile(path).getData();
-    std::vector<float> parsed = ImageParserWithSizing(width, height, channels).parseFloatsGreyScale(image);
-    return parsed;
+	cv::Mat image = ImageFileIO().readFile(path).getData();
+	std::vector<float> parsed = ImageParserWithSizing(width, height, channels).parseFloatsGreyScale(image);
+	return parsed;
 }
