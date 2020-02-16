adfcacee6a1b6ecc3c355c819066c72e5b09a6f9
Paul Stangel <paul.stangel@web.de>
2020-02-16 20:20:28 +0100

    Minor fix

--- ImplementationPhase/Model/NeuralNetworkModelModule/src/DenseLayer.cpp
+++ ImplementationPhase/Model/NeuralNetworkModelModule/src/DenseLayer.cpp
@@ -68,6 +68,6 @@ TENSOR(float) DenseLayer::backprob(TENSOR(float) feedback, float learningrate)
 
 void DenseLayer::setMode(DeviceType device, cl_int deviceID) {
   if (device == DeviceType::CPP) {
-    layerStrategy = DenseLayerCPP(&weights, &bias, &learningRate, &inputSize, &outputSize);
+    layerStrategy = DenseLayerCPP(this, inputSize, outputSize);
   }
 }
