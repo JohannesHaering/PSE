39b820e721a2f83a66ceb11430c3ac0a8a3244cf
JohannesHaring <johannes.haering99@gmx.de>
2020-03-06 08:56:49 +0100

    added test for max pool

---
+++ ImplementationPhase/test/Model/Parsing/LayerParsing/MaxPoolLayerParser_test.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <string>
+#include <list>
+#include <stdexcept>
+
+#include "LayerParser.hpp"
+#include "LayerType.hpp"
+#include "NetworkLayer.hpp"
+#include "MaxPoolLayerParser.hpp"
+#include "MaxPoolLayer.hpp"
+
+TEST(LayerParserTest, valid)
+{
+  std::string in = "";
+NetworkLayer* layer = MaxPoolLayerParser().parse(in);
+  EXPECT_EQ(LayerType::POLLING, layer->getLayerType());
+}
+
+TEST(LayerBackParserTest, valid)
+{
+   MaxPoolLayer* layer = new MaxPoolLayer();
+   EXPECT_EQ("[maxpool]\n", MaxPoolLayerParser().parseBack(layer));
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
