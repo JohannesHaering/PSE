ce48ca9160fff769664205cc4a838759cc17c652
johanneshaering <johanneshaering>
2020-03-09 21:51:31 +0100

    Added correct grey parsin

--- ImplementationPhase/Model/ModelFacadePackage/src/ImageFacade.cpp
+++ ImplementationPhase/Model/ModelFacadePackage/src/ImageFacade.cpp
@@ -13,13 +13,29 @@
 #include <list>
 #include <vector>
 
+#include <iostream>
+
 cv::Mat ImageFacade::getImage(std::string path, int width, int height, int channelNumb)
 {
 	cv::Mat image = ImageFileIO().readFile(path).getData();
 	cv::Mat parsedImage;
-	if (width != 0)
-		parsedImage = ImageParserWithSizing(width, height, channelNumb).parse(image);
-	else
+	if (width != 0){
+    for (int y = 0; y < height; y++){
+      for(int x  = 0; x < width; x++){
+        std::cout << (float) image.data[y * height + x] << " | ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+    
+    parsedImage = ImageParserWithSizing(width, height, channelNumb).parse(image);
+    for (int y = 0; y < height; y++){
+      for(int x  = 0; x < width; x++){
+        std::cout << (float) image.data[y * height + x] << " | ";
+      }
+      std::cout << std::endl;
+    }
+  } else
 		parsedImage = image;
 	return parsedImage;
 }
--- ImplementationPhase/Model/ParsingPackage/src/ImageParserWithSizing.cpp
+++ ImplementationPhase/Model/ParsingPackage/src/ImageParserWithSizing.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
+#include <iostream>
+
 ImageParserWithSizing::ImageParserWithSizing(int width, int height, int channels) :  ImageParser(width, height, channels) {};
 
 cv::Mat ImageParserWithSizing::parse(cv::Mat toParse)
@@ -18,7 +20,25 @@ std::vector<float> ImageParserWithSizing::parseFloatsGreyScale(cv::Mat toParse) 
     std::vector<float> output = std::vector<float>();
     cv::Mat temp;
     cv::Size size(width, height);
+    cv::Mat greyMat;
+
+    cv::cvtColor(toParse, greyMat, CV_BGR2GRAY);    
+    toParse = greyMat;
+    for(int y = 0; y < height; y++){
+      for(int x = 0; x < width; x++){
+        std::cout <<(float) toParse.data[y * height + x] << "|";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl << std::endl;
     cv::resize(toParse, temp, size);
+    for(int y = 0; y < height; y++){
+      for(int x = 0; x < width; x++){
+        std::cout <<(float) toParse.data[y * height + x] << "|";
+      }
+      std::cout << std::endl;
+    }
+
 
     for (int i = 0; i < temp.total(); i++) {
         output.push_back(((float)temp.data[i]) / 255.0f);
