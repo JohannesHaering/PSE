26263288e364e6fd0e413372311087233d36ea69
JohannesHaring <johannes.haering99@gmx.de>
2020-03-11 20:54:49 +0100

    Fixup

--- ImplementationPhase/test/Model/ModelTest.cpp
+++ ImplementationPhase/test/Model/ModelTest.cpp
@@ -1,5 +1,3 @@
-#define TESTDIR _TESTDIR
-
 #include <gtest/gtest.h>
 #include "IO/IOPackageTest.hpp"
 #include "Parsing/ParsingPackageTest.hpp"
@@ -19,35 +17,35 @@ void SetupModelFacadeTests()
 {
     struct stat info;
 
-    if (stat(TESTDIR, &info) != 0)
+    if (stat("~/Documents/PSE/ImplementationPhase/build/testdata", &info) != 0)
     {
-        mkdir(TESTDIR, 0);
-        mkdir(TESTDIR + "\\readdir", 0);
-        mkdir(TESTDIR + "\\writedir", 0);
+        mkdir("~/Documents/PSE/ImplementationPhase/build/testdata", 0);
+        mkdir("~/Documents/PSE/ImplementationPhase/build/testdata/readdir", 0);
+        mkdir("~/Documents/PSE/ImplementationPhase/build/testdata/writedir", 0);
     }
     else
     {
-        if (stat(TESTDIR + "\\readdir", &info) != 0)
+        if (stat("~/Documents/PSE/ImplementationPhase/build/testdata/readdir", &info) != 0)
         {
-            mkdir(TESTDIR + "\\readdir", 0);
+            mkdir("~/Documents/PSE/ImplementationPhase/build/testdata/readdir", 0);
 
             auto io = ImageFileIO();
             cv::Mat mat(64, 64, CV_32F, cv::Scalar(0, 0, 0));
-            io.writeFile(Data<cv::Mat>(mat), TESTDIR + "\\readir\\testBMPWrite.bmp");
+            io.writeFile(Data<cv::Mat>(mat), "~/Documents/PSE/ImplementationPhase/build/testdata/readir/testBMPWrite.bmp");
 
             auto nns = std::list<NeuralNetwork>();
             auto paths = std::list<std::string>();
             NeuralNetwork network = NeuralNetwork("nn", 64, 64, 3);
             ReLuLayer *layer = new ReLuLayer();
             network.addLayer(layer);
-            auto path = TESTDIR + "\\readdir\\TestNeuralNetworkWrite.txt";
+            auto path = "~/Documents/PSE/ImplementationPhase/build/testdata/readdir/TestNeuralNetworkWrite.txt";
             nns.push_back(network);
             paths.push_back(path);
             auto succes = NeuralNetworkFacade().saveNeuralNetworks(nns, paths);
         }
-        if (stat(TESTDIR + "\\writedir", &info) !=)
+        if (stat("~/Documents/PSE/ImplementationPhase/build/testdata/writedir", &info) !=)
         {
-            mkdir(TESTDIR + "\\writedir", 0);
+            mkdir("~/Documents/PSE/ImplementationPhase/build/testdata/writedir", 0);
         }
     }
 }
@@ -56,28 +54,28 @@ void setupIOTests()
 {
   struct stat info;
 
-  if (stat(TESTDIR, &info) != 0)
+  if (stat("~/Documents/PSE/ImplementationPhase/build/testdata", &info) != 0)
   {
-    mkdir(TESTDIR, 0);
-    mkdir(TESTDIR + "\\readdir", 0);
-    mkdir(TESTDIR + "\\writedir", 0);
+    mkdir("~/Documents/PSE/ImplementationPhase/build/testdata", 0);
+    mkdir("~/Documents/PSE/ImplementationPhase/build/testdata/readdir", 0);
+    mkdir("~/Documents/PSE/ImplementationPhase/build/testdata/writedir", 0);
   }
   else
   {
-    if (stat(TESTDIR + "\\readdir", &info) != 0)
+    if (stat("~/Documents/PSE/ImplementationPhase/build/testdata/readdir", &info) != 0)
     {
-      mkdir(TESTDIR + "\\readdir", 0);
+      mkdir("~/Documents/PSE/ImplementationPhase/build/testdata/readdir", 0);
 
       auto io = TextFileIO();
-      io.writeFile(TESTDIR + "\\readdir\\testTXT.txt", Data<std::string>("foo"));
+      io.writeFile("~/Documents/PSE/ImplementationPhase/build/testdata/readdir/testTXT.txt", Data<std::string>("foo"));
 
       auto io = ImageFileIO();
       cv::Mat mat(64, 64, CV_32F, cv::Scalar(0, 0, 0));
-      io.writeFile(Data<cv::Mat>(mat), TESTDIR + "\\readir\\testBMPWrite.bmp");
+      io.writeFile(Data<cv::Mat>(mat), "~/Documents/PSE/ImplementationPhase/build/testdata/readdir/testBMPWrite.bmp");
     }
-    if (stat(TESTDIR + "\\writedir", &info) !=)
+    if (stat("~/Documents/PSE/ImplementationPhase/build/testdata/writedir", &info) !=)
     {
-      mkdir(TESTDIR + "\\writedir", 0);
+      mkdir("~/Documents/PSE/ImplementationPhase/build/testdata/writedir", 0);
     }
   }
 }
