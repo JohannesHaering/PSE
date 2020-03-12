ba5fa664d4da04f6b2b95878ad8aab32fa66a1e0
Paul Stangel <paul.stangel@web.de>
2020-03-12 14:48:40 +0100

    Cleaned up

--- ImplementationPhase/test/Controller/Inferencing/InferencingPackageTests.hpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "src/InferencerTest.hpp"
-#include "src/DistributorTest.hpp"
-#include "src/TopologyInferencerTest.hpp"
-#include "src/TrainingDistributorTest.hpp"
-#include "src/InferencingDistributorClassificationTest.hpp"
-#include "src/PagerTest.hpp"
-#include "src/NeuralNetworkSetterTest.hpp"
