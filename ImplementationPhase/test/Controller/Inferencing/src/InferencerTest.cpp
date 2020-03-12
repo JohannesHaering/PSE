234be00c48e3ca02f934928b9001408df5948bff
johanneshaering <johanneshaering>
2020-03-12 09:30:31 +0100

    Tests

--- ImplementationPhase/test/Controller/Inferencing/src/InferencerTest.cpp
+++ ImplementationPhase/test/Controller/Inferencing/src/InferencerTest.cpp
@@ -57,6 +57,7 @@ TEST_F(InferencerTest, SettingNeuralNetworkTest)
 
 int main(int argc, char* argv[])
 {
+//  QApplication a(argc, argv);
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
