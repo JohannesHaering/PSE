a27dc0834d51aa8b79ff34ad459f1734bcff3fa0
johanneshaering <johanneshaering>
2020-03-13 08:59:32 +0100

    Added check for 0 layers

--- ImplementationPhase/Controller/DispatcherModule/src/Executor.cpp
+++ ImplementationPhase/Controller/DispatcherModule/src/Executor.cpp
@@ -8,7 +8,9 @@ Executor::Executor(NeuralNetworkAdapter* neuralNetwork) : neuralNetwork(neuralNe
 
 
 TENSOR(float) Executor::execute(TENSOR(float) input) {
-
+    if (neuralNetwork->getLayerCount() == 0) {
+      throw std::invalid_argument( "Error, network has 0 layers." );
+    }
     TENSOR(float) tmp = input;
     TENSOR(float) output;
     for (std::list<NetworkLayer*>::iterator layer = neuralNetwork->begin(); layer != neuralNetwork->end(); layer++)
